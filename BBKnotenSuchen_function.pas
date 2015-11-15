1c5a00da7b593f7c5ca67b14d27e3f998cfca9c8
Carlo Di Fulco <carlo.difulco@gmail.com>
2015-11-15 13:29:06 +0100

    commit di prova

---
+++ BBKnotenSuchen_function.pas
@@ -0,0 +1,30 @@
+function BBKnotenSuchen (
+inZahl : integer;
+inRefWurzel : tRefBinBaum): tRefBinBaum;
+{ liefert fuer den Suchbaum, auf dessen Wurzel
+inRefWurzel zeigt, den Zeiger auf den Knoten,
+dessen Wert gleich inZahl ist }
+var
+Zeiger : tRefBinBaum;
+gefunden : boolean;
+begin
+Zeiger := inRefWurzel;
+gefunden := false;
+while (Zeiger <> nil) and not gefunden do
+begin
+if inZahl = Zeiger^.info then
+gefunden := true
+else { nicht gefunden, daher links oder rechts
+weitermachen }
+if inZahl < Zeiger^.info then
+Zeiger := Zeiger^.links
+else201
+6. Programmierkonzepte orientiert an Pascal (Teil 4)
+Zeiger := Zeiger^.rechts
+end; { while }
+{ Jetzt gilt Zeiger = nil oder gefunden = true.
+Falls gefunden = true, zeigt Zeiger auf den Knoten
+mit info-Komponente = inZahl, andernfalls hat
+Zeiger den Wert nil }
+BBKnotenSuchen := Zeiger
+end; { BBKnotenSuchen }
